95b870a9b211aea77dc73925bb45f06eaa2d6b2b
albenisolmos <albenisolmos88@gmail.com>
2024-03-02 18:11:58 -0400

    refactor: logic and tailwind class

--- src/components/Dropzone.svelte
+++ src/components/Dropzone.svelte
@@ -11,17 +11,6 @@
 
 	let files
 	let colorDashed = twColors.slate[300]
-
-	$: if ($state == STATES.DONE) {
-		colorDashed = "transparent"
-	} else {
-		colorDashed = twColors.slate[300]
-	}
-
-	$: if ($state == STATES.INITIAL) {
-		files = undefined
-	}
-
 	$: if (files && files.length > 0) {
 		uploadFile(files[0])
 	}
@@ -46,14 +35,14 @@
 </script>
 
 <Dashed
-	stroke={colorDashed}
-	class="[stroke-dasharray:20_40] [stroke-linecap:round] stroke-slate-300 dark:stroke-zinc-700 stroke-[0.3em] rounded-[2em] flex justify-center items-center w-full max-w-[55em] aspect-video"
+	bind:stroke={colorDashed}
+	class="[stroke-dasharray:20_40] [stroke-linecap:round] stroke-slate-300 dark:stroke-zinc-700 stroke-[0.2em] rounded-[2em] flex justify-center items-center h-[80%] aspect-video max-w-full"
 >
 	{#if $state == STATES.INITIAL}
 		<StateInitial bind:files />
 	{:else if $state == STATES.LOADING}
 		<StateLoading />
-	{:else if $state == STATES.DONE}
+	{:else}
 		<StateDone />
 	{/if}
 </Dashed>
